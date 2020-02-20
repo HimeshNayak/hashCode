73604d77dfc5a194fee390adca297dda47e2cbb5
HimeshNayak <30944790+HimeshNayak@users.noreply.github.com>
2020-02-21 01:07:44 +0530

    Update main.cpp
    cout hta diye

--- main.cpp
+++ main.cpp
@@ -31,29 +31,20 @@ int main()
     cin>>noOfBooks>>noOfLibs>>days;
     books B[noOfBooks];
     libs L[noOfLibs];
-    cout<<noOfBooks<<' '<<noOfLibs<<' '<<days<<endl;
     for (int i = 0; i < noOfBooks; i++)
     {
         B[i].id = i;
         cin>>B[i].score;
     }
-
-    for (int i = 0; i < noOfBooks; i++)
-    {
-        cout<<B[i].score<<' ';
-    }
-    cout<<endl;
     for (int i = 0; i < noOfLibs; i++)
     {
         L[i].id = i;
         cin>>L[i].nob;
         cin>>L[i].daysSign;
         cin>>L[i].noOfBooksPerDay;
-        cout<<L[i].nob<<' '<<L[i].daysSign<<' '<<L[i].noOfBooksPerDay<<endl;
         for (int j = 0; j < L[i].nob; j++)
         {
             cin>>L[i].whatBooks[j];
         }
     }
-
 }
